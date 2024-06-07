f2f2e945440ddf3047ab5106a214dc6efe1e2aab
Michael Eisenbraun <eisenbm@algonquincollege.com>
2024-06-07 09:14:12 -0400

    Creates final version

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
-﻿internal class Program
+﻿using Quizzes;
+
+internal class Program
 {
+  private static Quiz quiz = new Quiz(Helper.GetAvailableQuestions());
+  enum Action { 
+    Add = 1, 
+    Play, 
+    Exit
+  }
   private static void Main(string[] args)
   {
     bool isActive = true;
@@ -13,6 +21,60 @@
       Console.Write("Enter an action: ");
       
       bool isValid = int.TryParse(Console.ReadLine(), out int action);
+
+      switch ((Action)action)
+      {
+        case Action.Add:
+          Add();
+          break;
+        case Action.Play:
+          Play();
+          break;
+        case Action.Exit:
+          isActive = false;
+          break;
+        default:
+          Console.WriteLine("Not a valid response.");
+          break;
+      }
+    }
+  }
+
+  private static void Add ()
+  {
+    string? text = null;
+    string? answer = null;  
+    
+    while (string.IsNullOrWhiteSpace(text)) 
+    {
+      Console.Write("Enter the question: ");
+      text = Console.ReadLine();
+    }
+
+    while (string.IsNullOrWhiteSpace(answer))
+    {
+      Console.Write("Enter the answer: ");
+      answer = Console.ReadLine();
+    }
+
+    try 
+    {
+      quiz.AddQuestion(text, answer);
+    } 
+    catch (Exception e) 
+    {
+      Console.WriteLine(e.Message);
+    }
+  }
+
+  private static void Play () {
+    try 
+    {
+      quiz.Play();
+    }
+    catch (Exception e) 
+    {
+      Console.WriteLine(e.Message);
     }
   }
 }
---
+++ Quiz.cs
@@ -0,0 +1,57 @@
+﻿namespace Quizzes;
+
+public class Quiz
+{
+  // fields
+  private List<Question> questions;
+
+  // constructor
+  public Quiz (List<Question> questions)
+  {
+    this.questions = questions;
+  }
+
+  // methods
+  public void AddQuestion (string text, string answer)
+  {
+    if (questions.Exists(question => question.Text == text)) {
+      throw new Exception("This question has already been added to the quiz.");
+    }
+
+    questions.Add(new Question(text, answer));
+  }
+  
+  public void Play () 
+  {
+    if (questions.Count == 0)
+    {
+      throw new Exception("No questions in the quiz.");
+    }
+
+    var shuffled = Helper.Shuffle(questions);
+    int score = 0;
+
+    foreach (var question in shuffled)
+    {
+        string? response = null;
+
+        while (string.IsNullOrWhiteSpace(response)) 
+        {
+          Console.WriteLine(question.Text);
+          response = Console.ReadLine();
+        }
+        
+        if (question.CheckAnswer(response))
+        {
+            Console.WriteLine("Correct!");
+            score += 1;
+        }
+        else
+        {
+            Console.WriteLine($"Wrong! The correct answer is: {question.Answer}");
+        }
+    }
+
+    Console.WriteLine($"You got {score} out of {questions.Count} correct.");
+  }
+}
